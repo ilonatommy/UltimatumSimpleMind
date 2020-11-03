f775610261ab1f47751114983c09075f27a963a5
Ilona Tomkowicz <ilona.tomkowicz@capgemini.com>
2020-11-03 10:04:50 +0100

    All emotion movements implemented correctly.

--- control.pl
+++ control.pl
@@ -5,6 +5,7 @@ setPosition(Y,X) :-
 	% add the current one
 	asserta(position(Y,X)).
 
+% PRIVATE SECTION
 moveUp() :-
 	position(Y,X),
 	NY is Y-1,
@@ -25,6 +26,35 @@ moveLeft() :-
 	NX is X-1,
 	setPosition(Y,NX).
 
+moveUpOrLeft() :-
+	(
+		\+moveUp() ->
+		moveLeft();
+		callable(true)
+	).
+
+moveUpOrRight() :-
+	(
+		\+moveUp() ->
+		moveRight();
+		callable(true)
+	).
+
+moveDownOrLeft() :-
+	(
+		\+moveDown() ->
+		moveLeft();
+		callable(true)
+	).
+
+moveDownOrRight() :-
+	(
+		\+moveDown() ->
+		moveRight();
+		callable(true)
+	).
+
+% PUBLIC SECTION
 
 % towardsEmotion should be used with negation, e.g.: \+towardsHappy(2).
 % T is number of steps
@@ -62,4 +92,90 @@ towardsSad(T) :-
 	TN is T-1,
 	towardsSad(TN).
 
-% TODO: how to write other TowardsEmotion predicates?
+towardsDisgusted(T) :-
+	T > 0,
+	position(Y,_),
+	(	
+		Y>2 ->
+		moveUpOrLeft();
+		(
+			Y<2 ->
+			moveDownOrLeft();
+			moveLeft()
+		)
+	),
+	TN is T-1,
+	towardsDisgusted(TN).
+
+towardsSurprised(T) :-
+	T > 0,
+	position(Y,_),
+	(	
+		Y>2 ->
+		moveUpOrRight();
+		(
+			Y<2 ->
+			moveDownOrRight();
+			moveRight()
+		)
+	),
+	TN is T-1,
+	towardsSurprised(TN).
+
+towardsAngry(T) :-
+	T > 0,
+	position(Y,_),
+	(	
+		Y>6 ->
+		moveDownOrLeft();
+		(
+			Y<6 ->
+			moveUpOrLeft();
+			moveLeft()
+		)
+	),
+	TN is T-1,
+	towardsAngry(TN).
+
+towardsFearful(T) :-
+	T > 0,
+	position(Y,_),
+	(	
+		Y>6 ->
+		moveUpOrRight();
+		(
+			Y<6 ->
+			moveDownOrRight();
+			moveRight()
+		)
+	),
+	TN is T-1,
+	towardsSurprised(TN).
+
+towardsCalm(T) :-
+	T > 0,
+	position(Y,X),
+	(	
+		Y==4 ->
+		% we are at CALM row
+		(
+			X==2 ->
+			% we are already at CALM field
+			callable(true);
+			(
+				X<2 ->
+				moveRight();
+				moveLeft()
+			)
+		);
+		% we are not at CALM row
+		(
+			Y<4 ->
+			% we are above it
+			moveDown();
+			% we are below it
+			moveUp()
+		)		
+	),
+	TN is T-1,
+	towardsCalm(TN).
