493602209837aecaec18b1b2e3699a7b92b563d8
paaweel <pawel.gakan@gmail.com>
2020-11-20 16:52:36 +0100

    Use class for prolog interactions

--- ui_control/main.py
+++ ui_control/main.py
@@ -3,32 +3,38 @@ import os
 
 PROLOG_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'prolog'))
 
+class DecisionModule:
+    def __init__(self, prolog_path=PROLOG_PATH):
+        self.prolog = Prolog()
+        self.prolog.consult(os.path.join(prolog_path, 'load.pl'))
 
-def start_game(prolog):
-    init_pos = list(prolog.query("init_state(Y,X)"))[0]
-    print(init_pos["Y"], init_pos["X"])
-    return init_pos["Y"], init_pos["X"]
+    def start_game(self):
+        init_pos = list(self.prolog.query("init_state(Y,X)"))[0]
+        print(init_pos["Y"], init_pos["X"])
+        return init_pos["Y"], init_pos["X"]
 
-
-def humanOffer(prolog, y, x, offer, emoFace, emoVoice):
-    response = list(prolog.query("humanOffers({}, {}, {}, {}, {}, RobotOffer, RobotDecision, Y_out, X_out)"
-                                 .format(y, x, offer, emoFace, emoVoice)))[0]
-    return response["Y_out"], response["X_out"], response["RobotOffer"], response["RobotDecision"]
-
-
-def humanDecides(prolog, y, x, decision):
-    response = list(prolog.query("humanDecides({}, {}, {}, Y_out, X_out)".format(y, x, decision)))[0]
-    return response["Y_out"], response["X_out"]
+    def humanOffer(self, y, x, offer, emoFace, emoVoice):
+        response = list(self.prolog.query("humanOffers({}, {}, {}, {}, {}, RobotOffer, RobotDecision, Y_out, X_out)"
+                                     .format(y, x, offer, emoFace, emoVoice)))[0]
+        return response["Y_out"], response["X_out"], response["RobotOffer"], response["RobotDecision"]
 
 
-prolog = Prolog()
-prolog.consult(os.path.join(PROLOG_PATH, 'load.pl'))
-y, x = start_game(prolog)
-offer = 5
-emoFace = "disgusted"
-emoVoice = "sad"
-y, x, robotOffer, robotDecision = humanOffer(prolog, y, x, offer, emoFace, emoVoice)
-print(robotOffer, robotDecision)
-decision = "yes"
-y, x = humanDecides(prolog, y, x, decision)
-print(y, x)
+    def humanDecides(self, y, x, decision):
+        response = list(self.prolog.query("humanDecides({}, {}, {}, Y_out, X_out)".format(y, x, decision)))[0]
+        return response["Y_out"], response["X_out"]
+
+
+
+
+if __name__ == "__main__":
+    offer = 5
+    emoFace = "disgusted"
+    emoVoice = "sad"
+
+    dm = DecisionModule()
+    y, x = dm.start_game()
+    y, x, robotOffer, robotDecision = dm.humanOffer(y, x, offer, emoFace, emoVoice)
+    print(robotOffer, robotDecision)
+    decision = "yes"
+    y, x = dm.humanDecides(y, x, decision)
+    print(y, x)
